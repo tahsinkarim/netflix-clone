4472f8a27ebf5b0a710b712f0c56ce34a87f0709
Tahsin Karim <tahsinkarim.dev@gmail.com>
2023-02-18 22:18:55 +0600

    added image in thumbnail component and removed scroll bar

--- components/Row.tsx
+++ components/Row.tsx
@@ -16,8 +16,10 @@ const Row = ({ title, movies }: Props) => {
       </h2>
       <div className='group relative md:-ml-2'>
         <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
-        <div>
-          <Thumbnail />
+        <div className='flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:pt-2'>
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
         </div>
         <ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
       </div>
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings";
 
-const Thumbnail = () => {
-  return <div></div>;
+interface Props {
+  movie: Movie;
+}
+
+const Thumbnail = ({ movie }: Props) => {
+  return (
+    <div className='relative h-28 min-w-[180px] curser-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'>
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className='rounded-sm object-cover md:rounded'
+        fill
+        alt={movie.name}
+      />
+    </div>
+  );
 };
 
 export default Thumbnail;
